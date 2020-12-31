c552cb2efb797f08ace64ad5b357e680e3b7c468
Tomasz Łakomy <hello@tlakomy.com>
2020-12-31 11:50:07 +0100

    Added example lambda data source

--- cdk-backend/lib/constructs/api.ts
+++ cdk-backend/lib/constructs/api.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as path from 'path';
 
 type ApiProps = {};
@@ -29,5 +30,18 @@ export class Api extends cdk.Construct {
     new cdk.CfnOutput(this, 'GraphQLAPIKey', {
       value: api.apiKey || '',
     });
+
+    // Example of adding a lambda function data source
+
+    // const notesLambda = new lambda.Function(this, 'AppSyncNotesHandler', {
+    //   runtime: lambda.Runtime.NODEJS_12_X,
+    //   handler: 'appsync.handler',
+    //   code: lambda.Code.fromAsset('lambda-fns'),
+    // });
+
+    // const lambdaDataSource = api.addLambdaDataSource(
+    //   'lambdaDataSource',
+    //   notesLambda,
+    // );
   }
 }
